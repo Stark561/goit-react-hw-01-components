a417419bd7ff8569c50b48f5cbac679b6c21ebe4
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-05-23 18:07:37 +0300

    hw-1/3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-// import { FriendsList } from './Friends/FriendsList';
+import { FriendsList } from './Friends/FriendsList';
 // import { TransactionHistory } from './Transactions/TransactionHistory.jsx';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
-// import friends from './Friends/friends.json';
+import friends from './Friends/friends.json';
 // import transactions from './Transactions/transactions.json';
 
 export const App = () => {
@@ -13,8 +13,8 @@ export const App = () => {
     <>
       <Profile {...user} />
       <Statistics title="Upload stats" stats={data} />
-      {/* <FriendsList list={friends} />
-      <TransactionHistory items={transactions} />; */}
+      <FriendsList list={friends} />
+      {/* <TransactionHistory items={transactions} />; */}
     </>
   );
 };
